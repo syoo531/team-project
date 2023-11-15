51d644456c44162d4246bb5003fd40dbfb43fb3c
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-11-16 00:22:21 +0900

    fix: project settings

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello!" });
-});
+// mongoose
+//   .connect(process.env.MONGO_DB_URL)
+//   .then(() => {
+//     console.log("MongoDB에 연결되었습니다.");
+//   })
+//   .catch((error) => {
+//     console.error("MongoDB 연결 실패:", error);
+//   });
 
-app.listen(4000, () => {
-  console.log("server start!!");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening Port on ${process.env.PORT}`);
 });
---
+++ server/routes/userRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.post("/signup", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+module.exports = router;
