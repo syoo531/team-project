c0492ee0519d03499466caa5a005400ae39db38b
YEEUNLEE <amy000809@gmail.com>
2023-11-16 10:30:48 +0900

    first commit - change file structure

--- client/src/app/(main)/layout.js
+++ client/src/app/(main)/layout.js
@@ -1,11 +1,13 @@
 import BandBanner from "./components/bandBanner/BandBanner";
 import Header from "./components/header/Header";
+import MyPageHome from "./myPage/myPageHome/MyPageHome";
 
 export default function HomeLayout({ children }) {
   return (
     <div>
       <BandBanner />
       <Header />
+      <MyPageHome />
       {children}
     </div>
   );
