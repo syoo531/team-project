31dddc5de220c43b0685aee2dad520a82da774e6
[SW6] 김호진 <trr0373@gmail.com>
2023-11-16 10:13:35 +0900

    feat login

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,10 @@
 import "../styles/reset.scss";
 import "../styles/global.scss";
+import Header from "./components/header/Header";
+import BandBanner from "./components/bandBanner/BandBanner";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const metadata = {
   title: "pop.spot!",
@@ -9,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <BandBanner />
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
