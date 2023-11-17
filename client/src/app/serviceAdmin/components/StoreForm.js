debe828b12747ce64bcb062a9f2457da0a680d53
유승희 <syoo531@naver.com>
2023-11-17 11:01:49 +0900

    feat: revise to useRef

--- client/src/app/serviceAdmin/components/StoreForm.js
+++ client/src/app/serviceAdmin/components/StoreForm.js
@@ -1,7 +1,7 @@
 "use client";
 
 import "./styles.scss";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { imageUploader } from "./imageUploader";
@@ -9,17 +9,15 @@ import { imageUploader } from "./imageUploader";
 const StoreForm = ({ storeData, storeId }) => {
   const router = useRouter();
 
-  const formIntialState = {
-    name: storeData?.name || "",
-    brand: storeData?.brand || "",
-    category: storeData?.category || "",
-    address: storeData?.address || "",
-    location: storeData?.location || "",
-    summary: storeData?.summary || "",
-    description: storeData?.description || "",
-    start_date: storeData?.start_date || "",
-    end_date: storeData?.end_date || "",
-  };
+  const name = useRef();
+  const brand = useRef();
+  const category = useRef();
+  const address = useRef();
+  const location = useRef();
+  const summary = useRef();
+  const description = useRef();
+  const start_date = useRef();
+  const end_date = useRef();
 
   const imageInitialState = {
     main_image_url: "",
@@ -28,15 +26,31 @@ const StoreForm = ({ storeData, storeId }) => {
   };
 
   const existingImageState = {
-    main_image_url: storeData?.image?.main_image_url || null,
-    thumbnail_image_url: storeData?.image?.thumbnail_image_url || null,
-    detail_image_url: storeData?.image?.detail_image_url || null,
+    main_image_url: "",
+    thumbnail_image_url: "",
+    detail_image_url: "",
   };
 
-  const [form, setForm] = useState(formIntialState);
+  //const [form, setForm] = useState(formIntialState);
   const [newImages, setNewImages] = useState(imageInitialState);
   const [error, setError] = useState({});
   const [existingImage, setExistingImage] = useState(existingImageState);
+
+  useEffect(() => {
+    if (storeData) {
+      name.current.value = storeData.name || "";
+      brand.current.value = storeData.brand || "";
+      category.current.value = storeData.category || "";
+      address.current.value = storeData.address || "";
+      location.current.value = storeData.location || "";
+      summary.current.value = storeData.summary || "";
+      description.current.value = storeData.description || "";
+      start_date.current.value = storeData.start_date || "";
+      end_date.current.value = storeData.end_date || "";
+
+      setExistingImage(() => storeData.image);
+    }
+  }, [storeData]);
 
   // const client = new S3Client({
   //   region: "ap-southeast-2",
@@ -83,33 +97,46 @@ const StoreForm = ({ storeData, storeId }) => {
   //   return response;
   // };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
 
-    setForm((prevForm) => ({
-      ...prevForm,
-      [name]: value,
-    }));
-  };
+  //   setForm((prevForm) => ({
+  //     ...prevForm,
+  //     [name]: value,
+  //   }));
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      const formData = {
+        name: name.current.value,
+        brand: brand.current.value,
+        category: category.current.value,
+        address: address.current.value,
+        location: location.current.value,
+        summary: summary.current.value,
+        description: description.current.value,
+        start_date: start_date.current.value,
+        end_date: end_date.current.value,
+      };
+
       if (storeId) {
         const newImageUrl = await imageUploader(newImages, existingImage);
         console.log("updated image URLS", newImageUrl);
-        const formData = { ...form, ...newImageUrl };
+        const updatedFormData = { ...formData, ...newImageUrl };
         const { data } = await axios.patch(
           `http://localhost:4000/popupStore/${storeId}`,
-          formData
+          updatedFormData
         );
         console.log(data);
       } else {
-        const imageURL = await imageUploader(newImages);
-        const formData = { ...form, ...imageURL };
+        console.log(newImages);
+        const imageURL = await imageUploader(newImages, false);
+        const updatedFormData = { ...formData, ...imageURL };
         const { data } = await axios.post(
           `http://localhost:4000/popupStore`,
-          formData
+          updatedFormData
         );
         console.log("res from server", data);
       }
@@ -126,56 +153,31 @@ const StoreForm = ({ storeData, storeId }) => {
         <div>
           <label>
             팝업스토어 이름:
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-            />
+            <input type="text" name="name" ref={name} />
           </label>
         </div>
         <div>
           <label>
             브랜드 이름:
-            <input
-              type="text"
-              name="brand"
-              value={form.brand}
-              onChange={handleChange}
-            />
+            <input type="text" name="brand" ref={brand} />
           </label>
         </div>
         <div>
           <label>
             카테고리:
-            <input
-              type="text"
-              name="category"
-              value={form.category}
-              onChange={handleChange}
-            />
+            <input type="text" name="category" ref={category} />
           </label>
         </div>
         <div>
           <label>
             주소:
-            <input
-              type="text"
-              name="address"
-              value={form.address}
-              onChange={handleChange}
-            />
+            <input type="text" name="address" ref={address} />
           </label>
         </div>
         <div>
           <label>
             지역:
-            <input
-              type="text"
-              name="location"
-              value={form.location}
-              onChange={handleChange}
-            />
+            <input type="text" name="location" ref={location} />
           </label>
         </div>
         <div>
@@ -224,54 +226,37 @@ const StoreForm = ({ storeData, storeId }) => {
         <div>
           <label>
             소개:
-            <input
-              type="text"
-              name="summary"
-              value={form.summary}
-              onChange={handleChange}
-            />
+            <input type="text" name="summary" ref={summary} />
           </label>
         </div>
         <div>
           <label>
             설명:
-            <input
-              type="text"
-              name="description"
-              value={form.description}
-              onChange={handleChange}
-            />
+            <input type="text" name="description" ref={description} />
           </label>
         </div>
         <div>
           <label>
             이벤트 시작일:
-            <input
-              type="text"
-              name="start_date"
-              value={form.start_date}
-              onChange={handleChange}
-            />
+            <input type="text" name="start_date" ref={start_date} />
           </label>
         </div>
         <div>
           <label>
             이벤트 종료일:
-            <input
-              type="text"
-              name="end_date"
-              value={form.end_date}
-              onChange={handleChange}
-            />
+            <input type="text" name="end_date" ref={end_date} />
           </label>
         </div>
         <div>
-          <img style={{ width: "200px" }} src={existingImage.main_image_url} />
+          <img style={{ width: "200px" }} src={existingImage?.main_image_url} />
           <img
             style={{ width: "200px" }}
-            src={existingImage.thumbnail_image_url}
+            src={existingImage?.thumbnail_image_url}
           />
-          <img style={{ width: "200px" }} src={existingImage.detail_image_url} />
+          <img
+            style={{ width: "200px" }}
+            src={existingImage?.detail_image_url}
+          />
         </div>
         <button type="submit">Submit</button>
         <button type="button" onClick={() => router.push("/serviceAdmin")}>
