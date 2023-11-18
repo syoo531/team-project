5ef4ccd30ec304f8999100c9dfe0aeb72e6c65ed
유승희 <syoo531@naver.com>
2023-11-18 23:28:12 +0900

    fix: useRef 코드정리

--- client/src/app/serviceAdmin/components/StoreForm/StoreForm.js
+++ client/src/app/serviceAdmin/components/StoreForm/StoreForm.js
@@ -6,18 +6,30 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import { imageUploader } from "../imageUploader";
 
-const StoreForm = ({ storeData, storeId }) => {
+const StoreForm = ({
+  name,
+  brand,
+  category,
+  address,
+  location,
+  summary,
+  description,
+  start_date,
+  end_date,
+  image, //이미지 url 담긴 객체
+  storeId,
+}) => {
   const router = useRouter();
 
-  const name = useRef();
-  const brand = useRef();
-  const category = useRef();
-  const address = useRef();
-  const location = useRef();
-  const summary = useRef();
-  const description = useRef();
-  const start_date = useRef();
-  const end_date = useRef();
+  const nameRef = useRef(name || null)
+  const brandRef = useRef(brand || null)
+  const categoryRef = useRef(category || null)
+  const addressRef = useRef(address || null);
+  const locationRef = useRef(location || null);
+  const summaryRef = useRef(summary || null);
+  const descriptionRef = useRef(description || null);
+  const start_dateRef = useRef(start_date || null);
+  const end_dateRef = useRef(end_date || null);
 
   const imageInitialState = {
     main_image_url: "",
@@ -26,9 +38,9 @@ const StoreForm = ({ storeData, storeId }) => {
   };
 
   const existingImageState = {
-    main_image_url: "",
-    thumbnail_image_url: "",
-    detail_image_url: "",
+    main_image_url: image?.main_image_url || "",
+    thumbnail_image_url: image?.thumbnail_image_url || "",
+    detail_image_url: image?.detail_image_url || "",
   };
 
   //const [form, setForm] = useState(formIntialState);
@@ -36,36 +48,24 @@ const StoreForm = ({ storeData, storeId }) => {
   const [error, setError] = useState({});
   const [existingImage, setExistingImage] = useState(existingImageState);
 
-  useEffect(() => {
-    if (storeData) {
-      name.current.value = storeData.name || "";
-      brand.current.value = storeData.brand || "";
-      category.current.value = storeData.category || "";
-      address.current.value = storeData.address || "";
-      location.current.value = storeData.location || "";
-      summary.current.value = storeData.summary || "";
-      description.current.value = storeData.description || "";
-      start_date.current.value = storeData.start_date || "";
-      end_date.current.value = storeData.end_date || "";
-
-      setExistingImage(() => storeData.image);
-    }
-  }, [storeData]);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const formData = {
-        name: name.current.value,
-        brand: brand.current.value,
-        category: category.current.value,
-        address: address.current.value,
-        location: location.current.value,
-        summary: summary.current.value,
-        description: description.current.value,
-        start_date: start_date.current.value,
-        end_date: end_date.current.value,
+        name: nameRef.current.value,
+        brand: brandRef.current.value,
+        category: categoryRef.current.value,
+        address: addressRef.current.value,
+        location: locationRef.current.value,
+        summary: summaryRef.current.value,
+        description: descriptionRef.current.value,
+        start_date: start_dateRef.current.value,
+        end_date: end_dateRef.current.value,
       };
+      console.log("formData", formData)
+      console.log("newImages", newImages)
+      console.log("existingImage", existingImage)
+
 
       if (storeId) {
         const newImageUrl = await imageUploader(newImages, existingImage);
@@ -98,102 +98,147 @@ const StoreForm = ({ storeData, storeId }) => {
         <div>
           <label>
             팝업스토어 이름:
-            <input type="text" name="name" ref={name} />
+            <input
+              type="text"
+              name="name"
+              ref={nameRef}
+              defaultValue={nameRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             브랜드 이름:
-            <input type="text" name="brand" ref={brand} />
+            <input
+              type="text"
+              name="brand"
+              ref={brandRef}
+              defaultValue={brandRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             카테고리:
-            <input type="text" name="category" ref={category} />
+            <input
+              type="text"
+              name="category"
+              ref={categoryRef}
+              defaultValue={categoryRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             주소:
-            <input type="text" name="address" ref={address} />
+            <input
+              type="text"
+              name="address"
+              ref={addressRef}
+              defaultValue={addressRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             지역:
-            <input type="text" name="location" ref={location} />
+            <input
+              type="text"
+              name="location"
+              ref={locationRef}
+              defaultValue={locationRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             소개:
-            <input type="text" name="summary" ref={summary} />
+            <input
+              type="text"
+              name="summary"
+              ref={summaryRef}
+              defaultValue={summaryRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             설명:
-            <input type="text" name="description" ref={description} />
+            <input
+              type="text"
+              name="description"
+              ref={descriptionRef}
+              defaultValue={descriptionRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             이벤트 시작일:
-            <input type="text" name="start_date" ref={start_date} />
+            <input
+              type="text"
+              name="start_date"
+              ref={start_dateRef}
+              defaultValue={start_dateRef?.current || ""}
+            />
           </label>
         </div>
         <div>
           <label>
             이벤트 종료일:
-            <input type="text" name="end_date" ref={end_date} />
+            <input
+              type="text"
+              name="end_date"
+              ref={end_dateRef}
+              defaultValue={end_dateRef?.current || ""}
+            />
           </label>
         </div>
-        <div>
 
-        <div className="container-image-upload">
-          <label>
-            메인 이미지:
-            <input
-              type="file"
-              name="main_image_url"
-              onChange={(e) =>
-                setNewImages((cur) => ({
-                  ...cur,
-                  main_image_url: e.target.files[0],
-                }))
-              }
-            />
-          </label>
-          <br></br>
-          <label>
-            썸네일 이미지:
-            <input
-              type="file"
-              name="thumbnail_image_url"
-              onChange={(e) =>
-                setNewImages((cur) => ({
-                  ...cur,
-                  thumbnail_image_url: e.target.files[0],
-                }))
-              }
-            />
-          </label>
-          <br></br>
-          <label>
-            상세 이미지:
-            <input
-              type="file"
-              name="detail_image_url"
-              onChange={(e) =>
-                setNewImages((cur) => ({
-                  ...cur,
-                  detail_image_url: e.target.files[0],
-                }))
-              }
-            />
-          </label>
-        </div>
+        <div>
+          <div className="container-image-upload">
+            <label>
+              메인 이미지:
+              <input
+                type="file"
+                name="main_image_url"
+                onChange={(e) =>
+                  setNewImages((cur) => ({
+                    ...cur,
+                    main_image_url: e.target.files[0],
+                  }))
+                }
+              />
+            </label>
+            <br></br>
+            <label>
+              썸네일 이미지:
+              <input
+                type="file"
+                name="thumbnail_image_url"
+                onChange={(e) =>
+                  setNewImages((cur) => ({
+                    ...cur,
+                    thumbnail_image_url: e.target.files[0],
+                  }))
+                }
+              />
+            </label>
+            <br></br>
+            <label>
+              상세 이미지:
+              <input
+                type="file"
+                name="detail_image_url"
+                onChange={(e) =>
+                  setNewImages((cur) => ({
+                    ...cur,
+                    detail_image_url: e.target.files[0],
+                  }))
+                }
+              />
+            </label>
+          </div>
 
           <img style={{ width: "200px" }} src={existingImage?.main_image_url} />
           <img
