9cc948efa10400625390e6debafedf3a629d7dec
[SW6] 김호진 <trr0373@gmail.com>
2023-11-29 17:36:30 +0900

    fix error

--- client/src/app/(main)/layout.js
+++ client/src/app/(main)/layout.js
@@ -3,12 +3,12 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 export default function HomeLayout({ children }) {
-    return (
-        <div>
-            {/* <BandBanner /> */}
-            {/* <Header /> */}
-            {children}
-            {/* <Footer /> */}
-        </div>
-    );
+  return (
+    <div>
+      <BandBanner />
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 }
